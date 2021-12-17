1e014e77624da69510a4ecff4e23318cff9cdfd8
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 15:43:12 -0600

    Create classes for system objects

---
+++ PressGang.Core/System/ResourceLocation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PressGang.Core.System
+{
+    public interface ResourceLocation
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ PressGang.Core/System/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PressGang.Core.System
+{
+    public class Store : ResourceLocation
+    {
+        public Store()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
