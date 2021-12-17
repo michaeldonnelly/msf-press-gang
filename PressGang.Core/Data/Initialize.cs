f25fbde810376ef1b49c6156fc5760dc649c6361
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 16:20:45 -0600

    Add shell data initializer

---
+++ PressGang.Core/Data/Initialize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PressGang.Core.System;
+
+namespace PressGang.Core.Data
+{
+    public static class Initialize
+    {
+        public static void ResourceLocations()
+        {
+            new Store("Main");
+            new Store("Raid");
+            new Store("Blitz");
+            new Store("Arena");
+            new Store("War");
+
+            new Campaign("Heros Assemble");
+            new Campaign("Villains United");
+            new Campaign("Nexus");
+            new Campaign("Mystic Forces Rising");
+        }
+
+    }
+}
