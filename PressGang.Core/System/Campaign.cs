b1b511c57fdf7a5988948b9f64f3a67a37bf3cb9
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 16:20:23 -0600

    Add Campaign

---
+++ PressGang.Core/System/Campaign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PressGang.Core.System
+{
+    public class Campaign
+    {
+        public Campaign(string name)
+        {
+            Name = name;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
