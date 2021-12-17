d6185049b05d917d580ef62e886efe80c8b81849
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 17:14:39 -0600

    Add Orb

---
+++ PressGang.Core/System/Location/Orb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PressGang.Core.System.Location
+{
+    public class Orb : ResourceLocation
+    {
+        public Orb(string name) : base(name)
+        {
+        }
+    }
+}
