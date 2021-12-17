f2490f25c229bb999770b2a9a9342eb406379b90
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 17:17:47 -0600

    Add Opportunity

---
+++ PressGang.Core/System/Opportunity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PressGang.Core.System
+{
+    public class Opportunity
+    {
+        public Opportunity()
+        {
+        }
+
+        public Resource Resource { get; set; }
+
+        public ResourceLocation ResourceLocation { get; set; }
+    }
+}
