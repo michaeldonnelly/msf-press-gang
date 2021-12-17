89d3b890af61bf44625f95eb26557a06cfeeb358
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-17 15:14:48 -0600

    Created

---
+++ press-gang-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace press_gang_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
