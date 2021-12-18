d3a7ee64ee3ed45efcf24506f05ea1fcda7d02f3
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-18 00:58:09 -0600

    Set the name of a character shard based on the name of the character

--- PressGang.Core/System/CharacterShard.cs
+++ PressGang.Core/System/CharacterShard.cs
@@ -3,8 +3,10 @@ namespace PressGang.Core.System
 {
     public class CharacterShard : Resource
     {
-        public CharacterShard()
+        public CharacterShard(Character character)
         {
+            this.Name = character.Name + "Shard";
+            Character = character; 
         }
 
         public Character Character { get; set; }
