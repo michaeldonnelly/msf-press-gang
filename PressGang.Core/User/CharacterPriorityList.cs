6fcac026eb076b43200bb1e0c29f291ba3919dc4
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-18 01:17:59 -0600

    Shell of CharacterPriorityList

---
+++ PressGang.Core/User/CharacterPriorityList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using PressGang.Core.System;
+
+namespace PressGang.Core.User
+{
+    public class CharacterPriorityList
+    {
+        public CharacterPriorityList()
+        {
+        }
+
+        public Dictionary<int, Character> Characters { get; set; }
+
+        public Character[] PriorityForMode(Type gameMode)
+        {
+            return null;
+        }
+    }
+}
