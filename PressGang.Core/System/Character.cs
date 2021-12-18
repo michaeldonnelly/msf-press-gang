c707fe55ada6931700b500720c21b04081b850bc
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-18 00:53:35 -0600

    Character constructor takes a name

--- PressGang.Core/System/Character.cs
+++ PressGang.Core/System/Character.cs
@@ -3,8 +3,9 @@ namespace PressGang.Core.System
 {
     public class Character
     {
-        public Character()
+        public Character(string name)
         {
+            Name = name;
         }
 
         public int Id { get; set; }
