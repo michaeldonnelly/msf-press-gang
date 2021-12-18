f696e0b455c860cc76990a3cb436472f248f20ba
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-18 16:37:47 -0600

    Add DbContext

---
+++ PressGang.Core/DbContext/PressGangContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PressGang.Core.System.Mode;
+
+namespace PressGang.Core
+{
+    public class PressGangContext : DbContext
+    {
+        public PressGangContext(DbContextOptions<PressGangContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Campaign> Campaigns { get; set; }
+    }
+}
