61c827ed603babf6aa18f5527d30f9d44b546d16
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-18 15:41:48 -0600

    Method to build the nodes for a campaign

--- PressGang.Core/System/Mode/Campaign.cs
+++ PressGang.Core/System/Mode/Campaign.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using PressGang.Core.System.Location;
+
 namespace PressGang.Core.System.Mode
 {
     public class Campaign : GameMode
     {
-        public Campaign(string name) : base(name)
+        public Campaign(string name, int levels = 0) : base(name)
         {
+            if (levels != 0)
+            {
+                Levels = levels;
+                NodesPerLevel = 9;
+            }
+        }
+
+        public int Levels { get; set; }
+
+        public int NodesPerLevel { get; set; }
+
+        public List<CampaignNode> Nodes()
+        {
+            List<CampaignNode> nodes = new();
+            for (int level = 1; level <= Levels; level++)
+            {
+                for (int node = 1; node <= NodesPerLevel; node++)
+                {
+                    CampaignNode campaignNode = new(this, level, node);
+                    nodes.Add(campaignNode);
+                }
+            }
+
+            return nodes;
         }
     }
 }
