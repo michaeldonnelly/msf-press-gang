8333eaf4bfa70bed62993545344035cfe41488c0
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-20 14:47:32 -0600

    Update ResourceLocation to be able to store the name of the more specific Type

--- PressGang.Core/System/Location/ResourceLocation.cs
+++ PressGang.Core/System/Location/ResourceLocation.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 namespace PressGang.Core.System.Location
 {
-    public partial class ResourceLocation
+    public class ResourceLocation
     {
         public ResourceLocation(string name)
         {
             Name = name;
         }
 
+        public ResourceLocation(string name, Type type)
+        {
+            Name = name;
+            LocationType = type.Name;
+        }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string LocationType { get; set; }
+
+        public Type GetLocationType()
+        {
+            return Type.GetType(LocationType);
+        }
     }
 }
