aab0a606d7762463d0d9b215832872e5f010611c
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-20 11:39:34 -0600

    DbSet Characters

--- PressGang.Core/Data/PressGangContext.cs
+++ PressGang.Core/Data/PressGangContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using PressGang.Core.System;
 using PressGang.Core.System.Location;
 using PressGang.Core.System.Mode;
 
@@ -13,6 +14,7 @@ namespace PressGang.Core.Data
 
         public DbSet<Campaign> Campaigns { get; set; }
         public DbSet<CampaignNode> CampaignNodes { get; set; }
+        public DbSet<Character> Characters { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
