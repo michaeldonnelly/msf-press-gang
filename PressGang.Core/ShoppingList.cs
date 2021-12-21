befe9df12661bcd7587699bba5a0c21625b4c189
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-20 19:18:24 -0600

    LoadResourceList

--- PressGang.Core/ShoppingList.cs
+++ PressGang.Core/ShoppingList.cs
@@ -29,21 +29,84 @@ namespace PressGang.Core
                 };
                 context.SaveChanges();
             }
-
-            LoadPriorities();
         }
 
-        private void LoadPriorities()
-        {
-            _priorities = _context.Priorities.Where<Priority>(p => p.User == _user).ToList<Priority>();
-        }
 
         public void RemoveCharacter(Character character)
         {
-            //Resource shard = 
+            Priority priority = FindCharacterPriority(character);
+            if (priority != null)
+            {
+                _context.Priorities.Remove(priority);
+                _context.SaveChanges();
+            }
         }
 
+        public void AddCharacter(Character character, int priorityLevel)
+        {
+            Resource shard = LookUp.Shard(_context, character);
+            Priority oldPriority = FindCharacterPriority(shard);
+            if (oldPriority != null)
+            {
+                if (oldPriority.PriorityLevel == priorityLevel)
+                {
+                    return;
+                }
+                else
+                {
+                    _context.Priorities.Remove(oldPriority);
+                }
+            }
 
+            Priority newPriority = new(_user, shard, priorityLevel);
+            _context.Add(newPriority);
+        }
+
+        private Priority FindCharacterPriority(Character character)
+        {
+            Resource shard = LookUp.Shard(_context, character);
+            return FindCharacterPriority(shard);
+        }
+
+        private Priority FindCharacterPriority(Resource shard)
+        { 
+            try
+            {
+                Priority priority = _context.Priorities.First(p =>
+                    (p.User == _user)
+                    && (p.Resource == shard)
+                );
+                return priority;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        //public List<Opportunity> ListCampaignOpportunities(LocationType locationType)
+        //{
+             
+
+        //}
+
+        private Dictionary<int, List<Resource>> LoadResourceList()
+        {
+            Dictionary<int, List<Resource>> resourceList = new();
+            foreach (Priority priority in _context.Priorities.Where<Priority>(p => p.User == _user))
+            {
+                if (resourceList.ContainsKey(priority.PriorityLevel))
+                {
+                    resourceList[priority.PriorityLevel].Add(priority.Resource);
+                }
+                else
+                {
+                    List<Resource> resources = new() { priority.Resource };
+                    resourceList.Add(priority.PriorityLevel, resources);
+                }
+            }
+            return resourceList;
+        }
 
     }
 }
