1924ad5ce4d8f44ad4e9b7f1b7bd39b20d7a861c
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-20 18:07:01 -0600

    User -> DynamicModels

--- PressGang.Core/Data/Initialize.cs
+++ PressGang.Core/Data/Initialize.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using PressGang.Core.StaticModels;
-using PressGang.Core.User;
+using PressGang.Core.DynamicModels;
 
 namespace PressGang.Core.Data
 {
