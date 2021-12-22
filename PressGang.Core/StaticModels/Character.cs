bfa3364928372f14a70650d4278e8dadd04701a1
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-22 16:22:06 -0600

    Character.ToString

--- PressGang.Core/StaticModels/Character.cs
+++ PressGang.Core/StaticModels/Character.cs
@@ -11,5 +11,10 @@ namespace PressGang.Core.StaticModels
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
