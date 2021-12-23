61c0a955f50d1bd4ee4460c83e8e388dec148f7e
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-22 18:39:27 -0600

    Discord IDs are ulongs

--- PressGang.Bot/DiscordOptions.cs
+++ PressGang.Bot/DiscordOptions.cs
@@ -10,6 +10,6 @@ namespace PressGang.Bot
         /// <summary>
         /// The Discord User ID of the person who owns the bot.  This determines who gets error messages.
         /// </summary>
-        public int OwnerUserId { get; set; } 
+        public ulong OwnerUserId { get; set; } 
     }
 }
