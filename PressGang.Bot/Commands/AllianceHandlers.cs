dfb855a64ebc105ace08f5417a09de4ce9504c8d
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-25 12:26:00 -0600

    Raid Lanes command

--- PressGang.Bot/Commands/AllianceHandlers.cs
+++ PressGang.Bot/Commands/AllianceHandlers.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Options;
+
 namespace PressGang.Bot.Commands
 {
-    public class AllianceHandlers
+    public class AllianceHandlers : HandlerCore
     {
-        public AllianceHandlers()
+        public AllianceHandlers(IOptions<DiscordOptions> options) : base(options)
+        { }
+
+        [Command("lanes")]
+        [Description("Display raid lane maps")]
+        [Aliases("lane")]
+        public async Task LanesCommand(CommandContext ctx, string raid = "all", int level = 0)
         {
+            List<RaidLane> raidLanes = _options.AllianceData.Maps.RaidLanes.Where(rl => RaidLaneMatchesQuery(rl, raid, level)).ToList();
+            if (raidLanes.Count == 0)
+            {
+                string response = "We don't have that lane defined yet.";
+                await DiscordUtils.Respond(ctx, response);
+            }
+            else
+            {
+                foreach (RaidLane lane in raidLanes)
+                {
+                    string title = $"{lane.Raid} {lane.Level}";
+                    await DiscordUtils.Respond(ctx, title, lane.MapUrl);
+                }
+            }
         }
+
+        private bool RaidLaneMatchesQuery(RaidLane raidLane, string raid, int level)
+        {
+            if (raid == "all")
+            {
+                return true;
+            }
+
+            if (raid != raidLane.Raid)
+            {
+                return false;
+            }
+
+            if (level == 0)
+            {
+                return true;
+            }
+
+            return (level == raidLane.Level);
+        }
+
+
     }
 }
--- PressGang.Bot/Listener.cs
+++ PressGang.Bot/Listener.cs
@@ -47,6 +47,7 @@ namespace PressGang.Bot
             CommandsNextExtension commands = discordClient.UseCommandsNext(commandsNextConfiguration);
             commands.RegisterCommands<AdminHandlers>();
             commands.RegisterCommands<HelpHandlers>();
+            commands.RegisterCommands<AllianceHandlers>();
             if (discordOptions.EnablePressGang)
             {
                 commands.RegisterCommands<PressGangHandlers>();
