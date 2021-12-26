1923b4df9c60e2251c50e4ac61899465811865c8
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-26 15:50:16 -0600

    add some whitespace

--- PressGang.Core/StartUp.cs
+++ PressGang.Core/StartUp.cs
@@ -38,12 +38,14 @@ namespace PressGang.Core
         {
             IConfigurationBuilder builder = new ConfigurationBuilder();
             builder.AddJsonFile("appsettings.json");
+
             string environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
             if (!String.IsNullOrEmpty(environment))
             {
                 string environmentSpecificConfig = $"appsettings.{environment}.json";
                 builder.AddJsonFile(environmentSpecificConfig, optional: true);
             }
+
             return builder.Build();
         }
 
