ea3b4ce82395e20f90686e0e1da58cec9bd689ee
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-26 17:25:24 -0600

    report on prereqs for a character

---
+++ PressGang.Core/Reports/StaticReports.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PressGang.Core.DatabaseContext;
+using PressGang.Core.StaticModels;
+using PressGang.Core.UserModels;
+
+namespace PressGang.Core.Reports
+{
+    public static class StaticReports
+    {
+        public static List<Character> Prerequisites(PressGangContext context, Character character)
+        {
+            List<Character> dependsOn = new();
+            foreach (Prerequisite prerequisite in character.Prerequisites)
+            {
+                dependsOn.Add(prerequisite.DependsOn);
+            }
+            return dependsOn;
+        }
+    }
+}
