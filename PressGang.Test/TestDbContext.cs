94b083347e91b56db463e71b47920429e9415f66
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-28 11:59:23 -0600

    Create test project

---
+++ PressGang.Test/TestDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PressGang.Core.DatabaseContext;
+
+namespace PressGang.Test
+{
+    public static class TestDbContext
+    {
+        //public static PressGangContext PressGangContext()
+        //{
+        //    //DataAccessOptions options = new()
+        //    //{
+        //    //    ConnectionString = "in memory something something",
+        //    //    EnsureCreated = true,
+        //    //    ImportData = true
+        //    //};
+        //    //PressGangContext context = new(options);
+
+        //}
+
+
+
+    }
+}
---
+++ PressGang.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PressGang.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
