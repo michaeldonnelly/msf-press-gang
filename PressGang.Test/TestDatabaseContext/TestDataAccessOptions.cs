9da99bba78804d0756342ccc8e288eedcfeee047
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-28 12:28:50 -0600

    Start out easy - test getting stuff from DataAccessOptions

---
+++ PressGang.Test/TestDatabaseContext/TestDataAccessOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PressGang.Core;
+using PressGang.Core.DatabaseContext;
+
+namespace PressGang.Test.TestDatabaseContext
+{
+    [TestClass]
+    public class TestDataAccessOptions
+    {
+        private DataAccessOptions _dataAccessOptions;
+
+        [TestInitialize]
+        public void StartUp()
+        {
+            Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT",null);
+
+            StartUp startUp = new StartUp();
+            IConfiguration configuration = startUp.Configuration;
+            configuration.GetSection(DataAccessOptions.DataAccess).Bind(_dataAccessOptions);
+
+        }
+
+        [TestMethod]
+        public void EnvironmentIsNullForProd()
+        {
+            string environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+            Assert.IsNull(environment);
+        }
+
+        [TestMethod]
+        public void ConnectionStringStartsWithDataSource()
+        {
+            bool connectionStringStartsWithDataSource = _dataAccessOptions.ConnectionString.StartsWith("Data Source");
+            Assert.IsTrue(connectionStringStartsWithDataSource);
+        }
+
+        [TestMethod]
+        public void ConnectionStringIsSqliteDb()
+        {
+            bool connectionStringIsSqliteDb = _dataAccessOptions.ConnectionString.EndsWith("pressgang.sqlite3");
+            Assert.IsTrue(connectionStringIsSqliteDb);
+        }
+
+
+
+    }
+}
