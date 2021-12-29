fc021f29ec85af42167fef939aecb1d946552f37
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-29 01:55:53 -0600

    Start testing prereqs (Iron Man)

--- PressGang.Test/ReportsTest/StaticReportsTest.cs
+++ PressGang.Test/ReportsTest/StaticReportsTest.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PressGang.Core;
 using PressGang.Core.DatabaseContext;
+using PressGang.Core.DatabaseOperations;
 using PressGang.Core.Reports;
+using PressGang.Core.StaticModels;
 using System;
+using System.Collections.Generic;
 
 namespace PressGang.Test.ReportsTest
 {
@@ -25,6 +28,45 @@ namespace PressGang.Test.ReportsTest
             PressGangContext context = InMemoryDatabase.GetContext();
             int characterRows = StaticReports.RowsInTable(context, "Characters");
             Assert.IsTrue(characterRows > 100, characterRows.ToString());
+        }
+
+        private List<string> Prereqs(string characterName)
+        {
+            PressGangContext context = InMemoryDatabase.GetContext();
+            Character character = LookUp.Character(context, characterName);
+            List<string> prereqs = StaticReports.Unlocks(context, character);
+            return prereqs;
+        }
+
+        [TestMethod]
+        public void IronMan()
+        {
+            List<string> prereqs = Prereqs("Iron Man");
+            List<string> expectedList = new()
+            {
+                "Agent Coulson",
+                "Black Widow",
+                "Captain America",
+                "Hawkeye",
+                "Nick Fury",
+                "Quake",
+                "S.H.I.E.L.D. Assault",
+                "S.H.I.E.L.D. Medic",
+                "S.H.I.E.L.D. Operative",
+                "S.H.I.E.L.D. Security",
+                "S.H.I.E.L.D. Trooper"
+            };
+
+            foreach (string expected in expectedList)
+            {
+                AssertListContains(prereqs, expected);
+            }
+        }
+
+        private void AssertListContains(List<string> actual, string expected)
+        {
+            bool listContainsExpectedString = actual.Contains(expected);
+            Assert.IsTrue(listContainsExpectedString, $"{expected} is in list");
         }
 
 
