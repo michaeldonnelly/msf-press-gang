292f17b38eaae3cc3f1e48b1cbf72e63e02f01d5
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-30 15:08:35 -0600

    symbolic link to update script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,4 +1,4 @@
-if [ ! -d $"msf-press-gang" ]
+  if [ ! -d $"msf-press-gang" ]
 then
   mkdir msf-press-gang
   git clone https://github.com/michaeldonnelly/msf-press-gang --no-checkout msf-press-gang --depth 1
@@ -7,21 +7,21 @@ then
   echo Data > .git/info/sparse-checkout
   echo "PressGang.Core/appsettings*.json" >> .git/info/sparse-checkout
   echo Import >> .git/info/sparse-checkout
-  echo deploy >> .git/info/sparse-checkout
+  echo Deploy >> .git/info/sparse-checkout
   git read-tree -m -u HEAD
   cd ..
 
   echo
   echo "Making directory for zola-test"
   mkdir zola-test
-  cp msf-press-gang/PressGang.Core/appsettings.Development.json zola/
+  cp -r msf-press-gang/PressGang.Core/appsettings.Development.json zola/
 
   echo
   echo "Making directory for zola"
   mkdir zola
-  cp msf-press-gang/PressGang.Core/appsettings.json zola/
+  cp -r msf-press-gang/PressGang.Core/appsettings.json zola/
 
-
+  ln -s msf-press-gang/Deploy/update-files.sh ./update-files.sh
 fi
 
-./update.sh
+./update-files.sh
