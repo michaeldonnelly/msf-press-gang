e6028560b7e999909b869ce3201dd85c5cd5c328
michaeldonnelly <donnelly@foolproofplan.net>
2021-12-31 01:05:25 -0600

    case for deploy folder

--- Deploy/update-files.sh
+++ Deploy/update-files.sh
@@ -3,6 +3,6 @@ cd msf-press-gang
 git pull
 cd ..
 echo "Copying to zola-test"
-cp -r msf-press-gang/deploy/test-template/* zola-test
+cp -r msf-press-gang/Deploy/test-template/* zola-test
 echo "Copying to zola"
-cp -r msf-press-gang/deploy/prod-template/* zola
+cp -r msf-press-gang/Deploy/prod-template/* zola
