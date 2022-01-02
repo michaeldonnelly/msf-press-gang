683b0d3c0a727a62f0c902122afd52ab559149bf
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-02 13:15:35 -0600

    shell WantListHandler

---
+++ PressGang.Bot/Commands/WantListHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
+using PressGang.Core;
+using PressGang.Core.DatabaseContext;
+using PressGang.Core.DatabaseOperations;
+using PressGang.Core.Reports;
+using PressGang.Core.StaticModels;
+using PressGang.Core.UserModels;
+
+namespace PressGang.Bot.Commands
+{
+    public class WantListHandler : HandlerCore
+    {
+
+        public PressGangContext PressGangContext { private get; set; }
+
+        public WantListHandler(IOptions<DiscordOptions> options) : base(options)
+        { }
+      
+    }
+}
