296ca4454da862dfab30f73f6ef1da7f94a7b3d1
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-02 14:01:18 -0600

    Shell GoalReports

---
+++ PressGang.Core/Reports/GoalReports.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PressGang.Core.DatabaseContext;
+using PressGang.Core.StaticModels;
+using PressGang.Core.UserModels;
+
+namespace PressGang.Core.Reports
+{
+    public class GoalReports
+    {
+        public GoalReports()
+        {
+        }
+    }
+}
