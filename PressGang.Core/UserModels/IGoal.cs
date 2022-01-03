7a8d0f7953b3ce760950eadba64ad9a601f02f7b
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-03 17:17:09 -0600

    remove dupe properties

--- PressGang.Core/UserModels/IGoal.cs
+++ PressGang.Core/UserModels/IGoal.cs
@@ -3,10 +3,6 @@ namespace PressGang.Core.UserModels
 {
     public interface IGoal : IPressGangRecord
     {
-        //public int Id { get; set; }
-
-        //public string Name { get; set; }
-
         public int UserId { get; set; }
 
         public User User { get; set; }
