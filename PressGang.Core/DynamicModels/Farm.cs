67de4477de9f16a99ab63947fc7b25474f5993f5
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-04 21:00:39 -0600

    add Farm.cs

---
+++ PressGang.Core/DynamicModels/Farm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PressGang.Core.StaticModels;
+using PressGang.Core.UserModels;
+
+namespace PressGang.Core.DynamicModels
+{
+    public class Farm
+    {
+        public Farm()
+        {
+        }
+
+        public int Priority { get; set; }
+
+        public Resource Resource { get; set; }
+
+        public Opportunity Opportunity { get; set; }
+    }
+}
