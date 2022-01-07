d111835cb21b32fed77427303a64599bc68b2fbf
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-06 22:14:23 -0600

    Add a goal for a char in heroes and make sure it comes out

--- PressGang.Test/ViewModelsTest/ShoppingListTest.cs
+++ PressGang.Test/ViewModelsTest/ShoppingListTest.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PressGang.Core.DatabaseContext;
 using PressGang.Core.DatabaseOperations;
 using PressGang.Core.Reports;
 using PressGang.Core.StaticModels;
 using PressGang.Core.UserModels;
+using PressGang.Core.ViewModels;
 
 namespace PressGang.Test.ViewModelsTest
 {
@@ -30,7 +32,22 @@ namespace PressGang.Test.ViewModelsTest
         public void DailyObjectives()
         {
             PressGangContext context = InMemoryDatabase.GetContext();
-            Assert.IsTrue(true);
+            User user = context.Users.Where(u => u.UserName == "Hawkshaw").FirstOrDefault();
+            Assert.IsNotNull(user);
+
+            Character bucky = LookUp.Character(context, "Bucky");
+            GoalOperations.AddYellowStarGoal(context, user, bucky);
+            ShoppingList shoppingList = new(context, user);
+
+            Opportunity opportunity = shoppingList.Heroes();
+            AssertOpportunity(opportunity, bucky, "Heroes");
+            
+        }
+
+        private void AssertOpportunity(Opportunity opportunity, Character character, string campaignName)
+        {
+            Assert.AreEqual(campaignName, opportunity.Location.Campaign.NickName);
+            Assert.AreSame(character, opportunity.Resource.Character);
         }
 
 
