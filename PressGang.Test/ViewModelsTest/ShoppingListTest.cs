05f7b4e846c24387f441c878adc2b19dcc4e4fff
michaeldonnelly <donnelly@foolproofplan.net>
2022-01-06 21:53:02 -0600

    ClassInit

--- PressGang.Test/ViewModelsTest/ShoppingListTest.cs
+++ PressGang.Test/ViewModelsTest/ShoppingListTest.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PressGang.Core.DatabaseContext;
+using PressGang.Core.DatabaseOperations;
+using PressGang.Core.Reports;
+using PressGang.Core.StaticModels;
+using PressGang.Core.UserModels;
+
 namespace PressGang.Test.ViewModelsTest
 {
+    [TestClass]
     public class ShoppingListTest
     {
-        public ShoppingListTest()
+        [ClassInitialize]
+        public static void Init(TestContext testContext)
         {
+            var keepalive = InMemoryDatabase.RawSqliteConnection();
+            keepalive.Open();
+            InMemoryDatabase.InitializeContext();
         }
+
+        private static void InitGoals()
+        {
+            PressGangContext context = InMemoryDatabase.GetContext();
+
+        }
+
+        [TestMethod]
+        public void DailyObjectives()
+        {
+            PressGangContext context = InMemoryDatabase.GetContext();
+            Assert.IsTrue(true);
+        }
+
+
     }
 }
