de705489c91e6e95d75001c35edea74b8b5b942c
Michael Donnelly <michaeld@hey.com>
2023-08-27 14:36:14 -0500

    add timestamps to tags

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,9 +1,7 @@
-docker build -t michaeldonnelly/zola-core -f Dockerfile-Core ..
-docker build -t michaeldonnelly/zola-console -f Dockerfile-Console ..
-docker build -t michaeldonnelly/zola-discord -f Dockerfile-Discord ..
-docker build -t michaeldonnelly/zola-web -f Dockerfile-Web ..
-
-
+timestamp=$(date -u +"%Y-%m-%d-%H%M")
+docker build .. --file Dockerfile-Core --tag michaeldonnelly/zola-core:$timestamp
+docker build .. --file Dockerfile-Discord --tag michaeldonnelly/zola-discord:$timestamp
+docker build .. --file Dockerfile-Web --tag michaeldonnelly/zola-web:$timestamp
 
 #  docker compose -f compose-console.yaml up
 #  docker compose -f compose-discord.yaml up
