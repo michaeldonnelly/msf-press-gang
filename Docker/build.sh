df3933af618d3ffaa70d4c104cc77682b9cdac90
Michael Donnelly <michaeld@hey.com>
2023-08-27 12:37:06 -0500

    Dockerfile and compose for Discord

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,2 +1,3 @@
 docker build -t michaeldonnelly/zola-core -f Dockerfile-Core ..
 docker build -t michaeldonnelly/zola-console -f Dockerfile-Console ..
+docker build -t michaeldonnelly/zola-discord -f Dockerfile-Discord ..
