e3c51ef1d5bf6ac2b2ad84619fb1b8e0f0ea30f9
Michael Donnelly <michaeld@hey.com>
2023-08-27 12:27:36 -0500

    working on getting secrets into containers

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,5 +1,2 @@
-# rm -rf build/*
-#dotnet publish .. -c Release -o ../build
-#dotnet publish .. -c Release
 docker build -t michaeldonnelly/zola-core -f Dockerfile-Core ..
-#docker build -t michaeldonnelly/zola-msfclient -f Dockerfile-MsfClient ..
+docker build -t michaeldonnelly/zola-console -f Dockerfile-Console ..
