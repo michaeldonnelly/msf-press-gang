0edcbff25c9cc7b16bca12803700d62d978fba9b
Michael Donnelly <michaeld@hey.com>
2023-09-11 13:36:34 -0700

    Serilog logging to console

--- src/Zola.Web/Program.cs
+++ src/Zola.Web/Program.cs
@@ -1,5 +1,12 @@
 using Zola.Database;
-using Zola.MsfClient; 
+using Zola.MsfClient;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
 
 ConfigurationBuilder configurationBuilder = new();
 configurationBuilder.AddUserSecrets<ApiSettings>();
@@ -10,9 +17,13 @@ IConfiguration config = configurationBuilder.Build();
 // TODO: do we even need these lines?
 DbSettings dbSettings = new(config);
 MsfDbContext dbContext = new(dbSettings);
-//
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MsfDbContext>();
 builder.Services.AddSingleton<IConfiguration>(config);
@@ -29,16 +40,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
+app.UseSerilogRequestLogging();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-//app.MapControllerRoute("callback", "{controller=Home}/{action=Callback}"); // TODO: nix
-
 app.Run();
+
+Log.Information("Shut down complete");
+Log.CloseAndFlush();
